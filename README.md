59bdf2754bb88eb9ece44c754f321c2a2a537190
vinceq <vinceq@usgs.gov>
2016-09-16 21:05:37 +0000

    Cleanup of interface, update README

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 # InShapefile
 Package to read a shapefile and determine if a set of points is inside it
 
+
+INSTALLATION
+
 NOTE: Remember to use mypy virtualenv
+
+pip install fiona
+pip install pyproj
+pip install shapely
+
+USAGE
+
+from inShapefile import loadShapefile,transformCoords,nativeProj
+
+# Returns a shapely MultiPolygon
+polyshape=loadShapefile('someshape.shp')
+
+# Returns a shapely Point
+point=transformCoords(lat,lon)
+
+# obj.intersects() from shapely
+if polyshape.intersects(point):
+	print('Yes, inside someshape!')
+
--- readshapefile.py
+++ readshapefile.py
@@ -4,6 +4,7 @@ import sys
 #import shapefile
 from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
+from shapely.geometry.multipolygon import MultiPolygon
 import argparse
 import json
 
@@ -12,7 +13,10 @@ import pyproj
 
 DEFAULT_SHAPEFILE='lib/ia/iowa_border.shp'
 
+nativeProj=None
+
 def loadShapefile(infile):
+
     with fiona.open(infile) as source:
         bounds=source.bounds
         crs=source.crs
@@ -23,34 +27,30 @@ def loadShapefile(infile):
             'crs':crs
             }
 
-    geojson={'type':'FeatureCollection','features':records,'properties':props}
-    return geojson
+    global nativeProj
+    nativeProj=crs['init']
+
+    polygons=[]
+    for feature in records:
+        featuretype=feature['geometry']['type']
+        if featuretype=='Polygon':
+            coordinates=feature['geometry']['coordinates']
+            for ring in coordinates:
+                polygons.append(Polygon(ring))
+
+    return MultiPolygon(polygons)
 
 
-def transformCoords(lat,lon,shapeproj):
-    print('Using',shapeproj)
+def transformCoords(lat,lon):
+
+    assert nativeProj, print("transformCoords: No nativeProj, run loadShapefile first!")
 
     proj0=pyproj.Proj(init='epsg:4326')  # Mercator lat/lon
-    proj1=pyproj.Proj(init=shapeproj) # IA UTM
+    proj1=pyproj.Proj(init=nativeProj) # IA UTM
 
     (x,y)=pyproj.transform(proj0,proj1,lon,lat)
     return Point(x,y)
 
-
-def inShape(point,jsonshape):
-    isin=False
-
-    for feature in jsonshape['features']: 
-        featuretype=feature['geometry']['type']
-
-        if featuretype=='Polygon':
-            polygons=feature['geometry']['coordinates']
-            for polypts in polygons:
-                polygon=Polygon(polypts)
-                if polygon.intersects(point):
-                    return True
-
-    return False
 
 if __name__=='__main__':
 
@@ -68,13 +68,12 @@ if __name__=='__main__':
 
     args=parser.parse_args()
 
-    jsonshape=loadShapefile(args.shapefile)
-    shapeproj=jsonshape['properties']['crs']['init']
-    point=transformCoords(args.lat,args.lon,shapeproj)
+    polyshape=loadShapefile(args.shapefile)
 
-    print('%s,%s is now %s %s.' % (args.lat,args.lon,point.x,point.y))
+    point=transformCoords(args.lat,args.lon)
+#    print('%s,%s is now %s %s.' % (args.lat,args.lon,point.x,point.y))
 
-    if inShape(point,jsonshape):
+    if polyshape.intersects(point):
         print('Yes! in shape.')
     else:
         print('Nope, outside shape.')
